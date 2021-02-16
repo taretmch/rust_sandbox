fd7df0d8d73083d59e19eb2b70476e68c41af840
taretmch <tomochuri25@gmail.com>
2021-02-17 00:25:34 +0900

    Add variables and data types

---
+++ variables/target/debug/variables.d
@@ -0,0 +1,1 @@
+/Users/tomoki/src/rust/rust_sandbox/variables/target/debug/variables: /Users/tomoki/src/rust/rust_sandbox/variables/src/main.rs
---
+++ variables/target/rls/debug/deps/variables-a5ce3317a2e53288.d
@@ -0,0 +1,5 @@
+/Users/tomoki/src/rust/rust_sandbox/variables/target/rls/debug/deps/variables-a5ce3317a2e53288.rmeta: src/main.rs
+
+/Users/tomoki/src/rust/rust_sandbox/variables/target/rls/debug/deps/variables-a5ce3317a2e53288.d: src/main.rs
+
+src/main.rs:
